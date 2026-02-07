92bddbfb7e555cba4e8dac2527221dd1f69e2198
gustavofjacome <gustavojacome123@gmail.com>
2026-02-07 12:28:40 -0300

    exercicio 2 recursivo

--- src/exercicios/ex2.java
+++ src/exercicios/ex2.java
@@ -1,9 +1,13 @@
 package exercicios;
 
 import java.util.Scanner;
-// acutângulo = 3 lados sao iguais
-// retângulo =  2 lados sao iguais
-// obtusângulo = 
+
+//Faça um programa para calcular o Máximo Divisor Comum entre 2 números passados como
+//argumentos para a execução do programa. Observe as seguintes propriedades do MDC:
+// MDC(x, y) = MDC(x − y, y), se x > y;
+// MDC(x, y) = MDC(y, x);
+// MDC(x, x) = x.
+//Exemplo: MDC(3,5)=MDC(5,3)=MDC(2,3)=MDC(3,2)=MDC(1,2)=MDC(2,1)=MDC(1,1)=1
 
 
 public class ex2 {
@@ -12,9 +16,19 @@ public class ex2 {
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int c = scanner.nextInt();
 
+        System.out.println(MDC(a,b));
+    }
 
-
+    static int MDC(int x, int y) {
+        if (x == y) {
+            return x;
+        }
+        if (x > y) {
+            return MDC(x-y, y);
+        }
+        else {
+            return MDC(y, x);
+        }
     }
 }
